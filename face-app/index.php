2fc1ce2687fa97d6bbc3f23403ec0bc143e62b00
alexander8buga <alexander8buga@gmail.com>
2018-12-04 12:46:55 -0800

    deleted css and create only one file

--- face-app/index.php
+++ face-app/index.php
@@ -2,26 +2,25 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <link rel="stylesheet" href="css/styles.css" type="text/css">
         <title>Face Posts</title>
     </head>
     <body>
         <div id="container">
             <header>
-                <h1 id="indent"><span class="blue-text">Face</span> posts</h1>
+                <h1 >Face posts</h1>
             </header>
-            <div class="row">
-                <div class="col">
-                    <form action="register.php" method="POST">
+            <div>
+                <div>
+                    <form action="index.php" method="POST">
                         <section>
-                            <h4 id="indent">Write your name</h4>
+                            <h4>Write your name</h4>
                             <p >Enter your name</p>
                             <input  type="text" name="name" >
-                            <button class="btn" type="submit" >Post</button>
+                            <button class="btn" type="submit" name = "submit">Post</button>
                         </section>
                     </form>
                 </div>
-                <div class="col">
+                <div>
                     <section>
                         <p>Fake posts:</p>
                         <?php
@@ -65,6 +64,80 @@
                         } catch(PDOException $e){
                             die("ERROR: Could not prepare/execute query: $sql. " . $e->getMessage());
                         }
+                        
+                        // check if the botton submit got pressed
+                        if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['submit'])){
+                            submit();
+                        }
+                        
+                        // Attempt insert query execution
+                        function submit(){
+                            
+                        /* Attempt MySQL server connection. Assuming you are running MySQL
+                        server with default setting (user 'root' with no password) */
+                        try{
+                            $connection = new PDO("mysql:host=localhost;dbname=MyDBTest", "root", "");
+                            // Set the PDO error mode to exception
+                            $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                        } catch(PDOException $e){
+                            die("ERROR: Could not connect. " . $e->getMessage());
+                        }
+                            
+                        // Attempt insert query execution
+                        try{
+                            // Prepare an insert statement
+                            $sql = "INSERT INTO posts (name) VALUES (:name)";
+                            $stmt = $connection->prepare($sql);
+
+                            // Bind parameters to statement
+                            $stmt->bindParam(':name', $_REQUEST['name']);
+
+
+                            /* Execute  the statement to insert a row */
+                            $affectedRows = $stmt->execute();
+
+                            if ($affectedRows  > 0) {
+                                echo ("<SCRIPT LANGUAGE='JavaScript'>
+                                    window.alert('Your Name inserted successfully.')
+                                    window.location.href='index.php'
+                                    </SCRIPT>");
+                                exit();
+
+                            } else {
+                                echo ("Insertion failed");
+                            } 
+
+                            // Attempt select query execution
+                            try{
+                                $sql = "SELECT * FROM posts";  
+                                $result = $connection->query($sql);
+                                if($result->rowCount() > 0){
+                                    echo "<table>";
+                                        echo "<tr>";
+                                            echo "<th>id</th>";
+                                            echo "<th>name</th>";
+                                        echo "</tr>";
+                                    while($row = $result->fetch()){
+                                        echo "<tr>";
+                                            echo "<td>" . $row['id'] . "</td>";
+                                            echo "<td>" . $row['name'] . "</td>";
+                                        echo "</tr>";
+                                    }
+                                    echo "</table>";
+                                    // Free result set
+                                    unset($result);
+                                } else{
+                                    echo "No records matching your query were found.";
+                                }
+                            } catch(PDOException $e){
+                                die("ERROR: Could not able to execute $sql. " . $e->getMessage());
+                            }
+
+                            } catch(PDOException $e){
+                                die("ERROR: Could not prepare/execute query: $sql. " . $e->getMessage());
+                            }
+                        }
+
                             
                         // Close statement
                         unset($stmt);
@@ -78,6 +151,3 @@
         </div>
     </body>
 </html>
-
-
-
